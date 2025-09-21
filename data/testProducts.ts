e82da2026468016ee03424efb240798914f2f5d2
Justin Thomas <justinshane13@gmail.com>
2025-09-21 01:02:29 -0700

    pull test data into separate file

---
+++ data/testProducts.ts
@@ -0,0 +1,103 @@
+import { Product } from "@/types";
+
+export const testProducts: Product[] = [
+  {
+    name: 'Coca-Cola',
+    score: 12,
+    ingredients: [
+      "water",
+      "high fructose corn syrup",
+      "natural flavors"
+    ]
+  },
+  {
+    name: 'Lay’s Classic Potato Chips',
+    score: 28,
+    ingredients: [
+      "potatoes",
+      "vegetable oil",
+      "salt"
+    ]
+  },
+  {
+    name: 'Oreo Cookies',
+    score: 22,
+    ingredients: [
+      "sugar",
+      "unbleached enriched flour",
+      "palm oil",
+      "cocoa"
+    ]
+  },
+  {
+    name: 'Neutrogena Face Wash',
+    score: 70,
+    ingredients: [
+      "water",
+      "glycerin",
+      "cocamidopropyl betaine",
+      "salicylic acid"
+    ]
+  },
+  {
+    name: 'KitKat Bar',
+    score: 30,
+    ingredients: [
+      "milk chocolate",
+      "sugar",
+      "wheat flour",
+      "vegetable oil"
+    ]
+  },
+  {
+    name: 'Maybelline Lipstick',
+    score: 55,
+    ingredients: [
+      "castor seed oil",
+      "beeswax",
+      "paraffin",
+      "fragrance"
+    ]
+  },
+  {
+    name: 'Ben & Jerry’s Chocolate Fudge Brownie',
+    score: 25,
+    ingredients: [
+      "cream",
+      "skim milk",
+      "sugar",
+      "cocoa",
+      "brownie pieces"
+    ]
+  },
+  {
+    name: 'Head & Shoulders Shampoo',
+    score: 65,
+    ingredients: [
+      "water",
+      "sodium lauryl sulfate",
+      "zinc pyrithione",
+      "fragrance"
+    ]
+  },
+  {
+    name: 'Nature Valley Granola Bar',
+    score: 72,
+    ingredients: [
+      "whole grain oats",
+      "sugar",
+      "canola oil",
+      "honey"
+    ]
+  },
+  {
+    name: 'Dove Body Lotion',
+    score: 80,
+    ingredients: [
+      "water",
+      "glycerin",
+      "stearic acid",
+      "dimethicone"
+    ]
+  }
+]
